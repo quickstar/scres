bd713ddfd5ac21c8cdcea82afd3ce2c09b7788cf
Lukas Läderach <lukas.laederach@garaio.com>
2014-01-30 09:41:39 +0100

    Adding resolution changer

---
+++ scres/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace scres
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			string myDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			string scFolderPath = Path.Combine(myDocumentsPath, "StarCraft II");
+			string fullVariablesPath = Path.Combine(scFolderPath, "Variables.txt");
+			string variablesContent = File.ReadAllText(fullVariablesPath);
+			Console.ForegroundColor = ConsoleColor.Red;
+
+			if (variablesContent.Contains("height=1200"))
+			{
+				Console.WriteLine("Current Height: 1200");
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine("Changing to 1080");
+				variablesContent = variablesContent.Replace("height=1200", "height=1080");
+			}
+			else
+			{
+				Console.WriteLine("Current Height: 1080");
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine("Changing to 1200");
+				variablesContent = variablesContent.Replace("height=1080", "height=1200");
+			}
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			File.WriteAllText(fullVariablesPath, variablesContent);
+
+			Console.WriteLine("Variables.txt successfully changed");
+		}
+	}
+}
---
+++ scres/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("scres")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GARAIO AG")]
+[assembly: AssemblyProduct("scres")]
+[assembly: AssemblyCopyright("Copyright © GARAIO AG 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fdd5dae3-f345-45e3-92de-11b5c399aa4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
